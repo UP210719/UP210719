90fb556ef41d59fc4372a06958f5313ba80f2d0d
ESTUDIANTES <you@example.com>
2022-09-12 10:53:22 -0500

    int prog

---
+++ U1/01_Hola.cpp
@@ -0,0 +1,22 @@
+/*
+Date: 12/09/2022
+Author: Eber Horacio JImenez Mendoza
+Email: up210719@alumnos.upa.edu.mx
+Description: Mi primer programa en c++
+*/
+
+//------------ PREPOCESSOR DIRECTIVES ---------
+
+
+//Input/output Library between the screen and keyboard
+#include <iostream>  
+
+//---------- MAIN FUNCTION -------
+int main(){
+
+//Prints on screen a text 
+std::cout << "Hello guys! This is my first programm";
+
+//Return 0 means that the programm ends succesfully
+return 0;
+}
