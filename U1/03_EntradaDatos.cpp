50293ae5f1fc00fc51f8f88158db2efa6392dcac
ESTUDIANTES <you@example.com>
2022-09-21 10:45:21 -0500

    hola

--- U1/03_EntradaDatos.cpp
+++ U1/03_EntradaDatos.cpp
@@ -1,5 +1,5 @@
 /* Unidad 1. Entrada de datos
-   Autor: Luis Ernesto Anaya Tiscareño
+   Autor: Eber Horacio Jimenez Mendoza
    Fecha: 01/09/2022
    Descripción: Muestra la forma de ingresar tipos de datos por el usuario
 */
@@ -9,4 +9,30 @@
 //Libreria para el uso de printf y scanf
 #include <stdio.h>
 
-//Uso del namespace 
+//Uso del namespace para evitar el std::
+using namespace std;
+
+//Función principal de tipo entero
+int main()
+{
+    int entero;
+    float flotante;
+    double grande;
+    char caracter;
+    
+    cout << "Este programa muestra el ingreso de datos por el usuario. \n";
+    cout << "Ingresa un dato de tipo entero: ";
+    cin >> entero;
+    cout << "El dato ingresado es: " << entero << endl;
+    
+    cout << "=================" << endl;
+
+    printf("Usando la función printf y scanf\n");
+    printf("Ingresa un dato de tipo entero: ");
+    scanf("%d", &entero);
+    printf("El dato ingresado es: %i \n", entero);
+
+    return 0;
+
+
+}
