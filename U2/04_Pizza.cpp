f7fb4b2c32a54dd959f58f5c838daa1fc9175c55
UP210079 <up210079@alumnos.upa.edu.mx>
2022-09-30 09:21:16 -0500

    pizza

--- U2/04_Pizza.cpp
+++ U2/04_Pizza.cpp
@@ -8,7 +8,59 @@ ingredients it contains.
  Nivel
  Nivel puntuacion
  choose two ingredients
-
- less than 4 years old = free
- if ur kid have 4 years old or 18 years old have to paid $5 and if hes older than 18 years old ha ve to paid $10
 */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char pizza;
+    int ingrediente;
+    cout << "¿Quieres una pizza vegetariana? Todas nuestras pizzas llevan mozzarella y tomate\n Y/N \n";
+    cin >> pizza;
+
+    if (pizza == 'Y' || pizza == 'y')
+    {
+        cout << " Estos son tus ingredientes para tu pizza vegetariana \n¿Cual desdea agregar? : \n 1.Pimiento \n 2.Tofu \n Escoge un ingrediente extra para agregar a tu pizza: ";
+        cin >> ingrediente;
+
+         cout << "Tu pizza es vegetariana, tus ingredientes son:" << endl;
+
+        if (ingrediente == 1)
+        {
+            cout << "-Tu ingrediente extra para tu pizza sera: Pimiento " << endl;
+        }
+        else if (ingrediente == 2)
+        {
+            cout << "-Tu ingrediente extra para tu pizza sera: Tofu " << endl;
+        }
+
+       
+    }
+    else if (pizza == 'N' || pizza == 'n')
+    {
+        cout << "Estos son tus otros ingredientes para tu pizza no vegetariana: \n 1.Peperoni \n 2.Jamon \n 3.Salmon \n Escoge un ingrediente extra para agregar a tu pizza: ";
+        cin >> ingrediente;
+
+        if (ingrediente == 1)
+        {
+            cout << "-Tu ingrediente extra para tu pizza sera: Peperoni";
+        }
+        else if (ingrediente == 2)
+        {
+            cout << "-Tu ingrediente extra para tu pizza sera: Jamon";
+        }
+        else if (ingrediente == 3)
+        {
+            cout << "-Tu ingrediente extra para tu pizza sera: Salmon";
+        }
+    }
+    else
+    {
+        cout << "Solo se permite Y (es si) o N (es no)";
+    }
+
+   
+    return 0;
+}
