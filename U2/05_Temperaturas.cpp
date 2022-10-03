db0989cc86d06a71ee6ba411bf07469d4cbd48bf
UP210079 <up210079@alumnos.upa.edu.mx>
2022-10-03 10:31:04 -0500

    new work

--- U2/05_Temperaturas.cpp
+++ U2/05_Temperaturas.cpp
@@ -12,18 +12,28 @@ int main()
     int contador=0;
     float temperature;
     float tempAcum = 0;
-    float alta = ;
-    float baja = ;
+    float mintemperature = 9999;
+    float maxtemperature = -9999;
     do{
         cout<<"Give me the temperature: ";
         cin >> temperature;
+        if (temperature >= maxtemperature)
+        {
+            maxtemperature = temperature
+        }
+        if (temperature <= mintemperature)
+        {
+            mintemperature = temperature
+        }
         tempAcum += temperature;
         contador++;
     }while (contador < 6);
 
     cout<<"The average of temperature today is: " <<tempAcum/6 << endl;
-    cout<<"The highest temperature is: "; <<alta << endl;
-    cout<<"The lowest temperature is: "; <<baja << endl;
+    cout<<"The highest temperature is: "; <<maxtemperature << endl;
+    cout<<"The lowest temperature is: "; <<mintemperature << endl;
+
+    return 0;
 
 
 
