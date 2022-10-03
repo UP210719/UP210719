2fb1d06358af5714552c74dbaf3580aeef88e9bf
UP210079 <up210079@alumnos.upa.edu.mx>
2022-10-03 10:12:22 -0500

    Agregado

---
+++ U2/05_Temperaturas.cpp
@@ -0,0 +1,28 @@
+/* Unidad 2. Uso de ciclo Do While
+   Autor: Eber Horacio Jimenez Mendoza
+   Fecha: 03/10/2022
+   Descripción: Muestra el uso de la sentencia Do While
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int contador=0;
+    float temperature;
+    float tempAcum = 0;
+    do{
+        cout<<"Give me the temperature: ";
+        cin >> temperature;
+        tempAcum += temperature;
+        contador++;
+    }while (contador < 6);
+
+    cout<<"The average of temperature today is: " <<tempAcum/6 << endl;
+    cout<<"The highest temperature is: "; <<alta << endl;
+    cout<<"The lowest temperature is: "; <<baja << endl;
+
+
+
+}
