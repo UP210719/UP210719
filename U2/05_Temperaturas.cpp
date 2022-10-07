6ebdbba6c70db1c56fcacd92175356f3f0625702
UP210719 <112641698+UP210719@users.noreply.github.com>
2022-10-07 09:38:27 -0500

    Update 05_Temperaturas.cpp

--- U2/05_Temperaturas.cpp
+++ U2/05_Temperaturas.cpp
@@ -3,44 +3,33 @@
    Fecha: 03/10/2022
    Descripción: Muestra el uso de la sentencia Do While
 */
-
-#include <iostream>
+include <iostream>
 using namespace std;
 
 int main()
 {
     int contador=0;
     float temperatura;
-    float mintemperature=9999;
-    float maxtemperature=-9999;
+    float temperaturaBaja=9999;
+    float temperaturaAlta=-9999;
     float tempAverage = 0;
     do{
         cout <<"Give me the temperature: ";
         cin >> temperatura;
         tempAverage += temperatura;
         contador++;
-<<<<<<< HEAD
-    }while (contador < 6);
-
-    cout<<"The average of temperature today is: " <<tempAcum/6 << endl;
-    cout<<"The highest temperature is: "; << maxtemperature << endl;
-    cout<<"The lowest temperature is: "; << mintemperature << endl;
-
-    return 0;
-=======
-        if(temperatura <= mintemperature)
-        mintemperature = temperatura;
-        if(temperatura >= maxtemperature)
-        maxtemperature = temperatura;
->>>>>>> 43059fd913f0345e85a1648625aab74a55de64d4
+        if(temperatura <= temperaturaBaja)
+        temperaturaBaja = temperatura;
+        if(temperatura >= temperaturaAlta)
+        temperaturaAlta = temperatura;
 
 
     }while(contador < 6);
     
 
     cout <<"The average of temperature today is: " << tempAverage/6 <<endl;
-    cout << "The lowest temperature today is: " << mintemperature <<endl;
-    cout << "The highest temperature today is: " << maxtemperature <<endl;
-    
+    cout << "The lowest temperature today is: " << temperaturaBaja <<endl;
+    cout << "The highest temperature today is: " << temperaturaAlta <<endl; 
     return 0;
 }
+
