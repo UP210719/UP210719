b919e044a56857078bfe1ab5417ef14596945f4d
hola <hola>
2022-10-12 10:26:01 -0500

    new work

--- U2/08_TablasMultiplicar.cpp
+++ U2/08_TablasMultiplicar.cpp
@@ -5,3 +5,24 @@ Date: 12/10/2022
 Description: Program to enter multiplication tables
 Last modification: 12/10/2022
 */
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number, multi, i, limite;
+    cout<<"Enter a number: ";
+    cin>>number;
+    cout<<"Enter a limit: ";
+    cin>>limite;
+
+    cout<<"-----------------------\n";
+
+    for(i=1; i<=limite; i++){
+        multi= number *1;
+        cout<<"|\t" <<i <<"\tx\t" <<number <<"\t=\t" <<multi <<"\t|\n";
+        cout<<"-----------------------\n"; 
+    }
+    return 0;
+
+
+}
