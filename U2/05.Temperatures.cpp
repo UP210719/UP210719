668eea84804a7286b393d0852c6076199f62102b
hola <hola>
2022-10-12 09:30:18 -0500

    new work

--- U2/05.Temperatures.cpp
+++ U2/05.Temperatures.cpp
@@ -3,7 +3,6 @@
    Fecha: 03/10/2022
    Descripción: Muestra el uso de la sentencia Do While
 */
-
 #include <iostream>
 using namespace std;
 
@@ -19,20 +18,10 @@ int main()
         cin >> temperatura;
         tempAverage += temperatura;
         contador++;
-<<<<<<< HEAD
-    }while (contador < 6);
-
-    cout<<"The average of temperature today is: " <<tempAcum/6 << endl;
-    cout<<"The highest temperature is: "; << maxtemperature << endl;
-    cout<<"The lowest temperature is: "; << mintemperature << endl;
-
-    return 0;
-=======
         if(temperatura <= mintemperature)
         mintemperature = temperatura;
         if(temperatura >= maxtemperature)
         maxtemperature = temperatura;
->>>>>>> 43059fd913f0345e85a1648625aab74a55de64d4
 
 
     }while(contador < 6);
@@ -40,7 +29,6 @@ int main()
 
     cout <<"The average of temperature today is: " << tempAverage/6 <<endl;
     cout << "The lowest temperature today is: " << mintemperature <<endl;
-    cout << "The highest temperature today is: " << maxtemperature <<endl;
-    
+    cout << "The highest temperature today is: " << maxtemperature <<endl; 
     return 0;
 }
